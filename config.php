2305523a65d14a096767462e19bcdc8b3ad66610
Ajay Rathore <rathorevirat131@gmail.com>
2023-02-16 12:17:20 +0530

    updated db details

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 $host = 'localhost';  
 $user = 'root';  
 $pass = ''; 
-$database = 'buildrr';
-define('ABSPATH',  str_replace('\\', '/', dirname( __FILE__ ) . '/') );
+$database = 'builderr';
 $conn = mysqli_connect($host, $user, $pass,$database);  
 if(!$conn)	{
 	die("Connection failed: " . mysqli_connect_error());
 }
-?>
+define( 'SITE_URL', 'http://localhost/buildrrd/');
+define( 'ABSPATH', str_replace('\\', '/', dirname( __FILE__ ) . '/') );
